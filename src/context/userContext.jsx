6360b4d7d03156ab4857504c5f8486aa58157c11
Vinicius <vinihenriqueh@gmail.com>
2022-08-07 20:42:36 -0300

    remoção de codigo não utilizado

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -63,23 +63,6 @@ function UserProvider({ children }) {
 
   };
 
-
-  // useEffect(() => {
-  //   api.get('/transactions')
-  //     .then(response => setNewTransactions(response.data));
-  // }, []);
-
-  // async function createTransaction(transaction) {
-
-  //   const response = await api.post('/new-transaction', transaction);
-
-  //   const newTransaction = response.data[0]
-
-  //   setNewTransactions([...newTransactions, newTransaction]);
-
-  // }
-
-
   return (
     <UserContext.Provider value={{ getUserData, getUserRepos, userRepos, getBranches, getCommits }}>
       {children}
